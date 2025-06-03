10c245011bfec78891985bf9a55a584ab42d9078
AKA-Slim-Shady <surya23110193@snuchennai.edu.in>
2025-06-03 19:41:03 +0530

    Add Backend-FCC project

---
+++ Backend-FCC/TimeStamp-Microservice/README.md
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+# Timestamp Microservice
+
+This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+=======
+# Surya-FCC-Backend
+
+Projects coded for the successful completion of FreeCodeCamp's "Backend Development and API's" certification
+>>>>>>> 8e59a8584b0a060a093dfc45bb7ce8d6aace011c
---
+++ Backend-FCC/TimeStamp-Microservice/index.js
@@ -0,0 +1,67 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+
+  if (!date) {
+    let ts = new Date();
+    return res.json({
+      unix: ts.getTime(),
+      utc: ts.toUTCString()
+    });
+  }
+
+  if (isNaN(Number(date))) {
+    let ts = new Date(date);
+    if (ts.toString() === "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    }
+    let unix = ts.getTime();
+    let utc = ts.toUTCString();
+    return res.json({
+      unix: unix,
+      utc: utc
+    });
+  } else {
+    let ts = new Date(Number(date));
+    if (ts.toString() === "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    }
+    let unix = ts.getTime();
+    let utc = ts.toUTCString();
+    return res.json({
+      unix: unix,
+      utc: utc
+    });
+  }
+});
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
